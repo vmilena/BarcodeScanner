a192856da628b3597555b61b9bf9f49f0616b504
Milena <94milena@gmail.com>
2019-10-23 21:17:59 +0200

    Photo manipulation changed
    Firebas Ml kit barcode recognition implemented

--- app/src/main/java/com/example/barcodedemo/ScannerActivity.java
+++ app/src/main/java/com/example/barcodedemo/ScannerActivity.java
@@ -1,14 +1,17 @@
 package com.example.barcodedemo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 
-import com.google.android.gms.tasks.OnFailureListener;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.ml.vision.FirebaseVision;
@@ -17,49 +20,94 @@ import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
+import javax.xml.transform.Source;
 
 import butterknife.ButterKnife;
 
+import static android.graphics.ImageDecoder.createSource;
+
 public class ScannerActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
         ButterKnife.bind(this);
-
         dispatchTakePictureIntent();
-
     }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
+
+    private File createImageFile() throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            Bitmap imageBitmap = BitmapFactory.decodeFile(currentPhotoPath);
+
+            FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
 
             FirebaseVisionBarcodeDetectorOptions options =
                     new FirebaseVisionBarcodeDetectorOptions.Builder()
-                    .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_ALL_FORMATS)
-                    .build();
+                            .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_ALL_FORMATS)
+                            .build();
 
             FirebaseVisionBarcodeDetector detector =
-                    FirebaseVision.getInstance().getVisionBarcodeDetector(
-                            );
-            FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
+                    FirebaseVision.getInstance().getVisionBarcodeDetector(options);
 
             Task<List<FirebaseVisionBarcode>> task = detector.detectInImage(image);
-            task.addOnSuccessListener(firebaseVisionBarcodes -> System.out.println(firebaseVisionBarcodes));
+            task.addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                @Override
+                public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                    for (FirebaseVisionBarcode barcode :
+                            firebaseVisionBarcodes) {
+                        System.out.println(barcode.getDisplayValue());
+                    }
+                }
+            });
         }
     }
 
